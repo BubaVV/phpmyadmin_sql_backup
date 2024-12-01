d67fa8db9628cb738161afe6d4c3c7f95a103114
BubaVV <vv@WIN-TR4JBTJC6N9>
2024-12-01 23:27:06 +0200

    Update for PHPMyAdmin 5.2
    Grab looks abandoned and not working anymore. Migrated it to lxml and
    tested a bit on recent phpmyadmin installation

--- phpmyadmin_sql_backup.py
+++ phpmyadmin_sql_backup.py
@@ -28,64 +28,103 @@ import datetime
 import os
 import re
 import sys
+from itertools import product
+from urllib.parse import urljoin
 
-import grab
+import requests
+from lxml import html
 
-__version__ = '2019-05-07.1'
+__version__ = '2024-12-01'
 
 CONTENT_DISPOSITION_FILENAME_RE = re.compile(r'^.*filename="(?P<filename>[^"]+)".*$')
 DEFAULT_PREFIX_FORMAT = r'%Y-%m-%d--%H-%M-%S-UTC_'
 
 
-def is_login_successful(g):
-    return g.doc.text_search("frame_content") or g.doc.text_search("server_export.php")
+def is_login_successful(tree):
+    hrefs = tree.xpath("//a/@href")
+    target_substrings = ["frame_content", "server_export.php", "index.php?route=/server/export"]
+    combinations = product(target_substrings, hrefs)
 
-
-def open_frame_if_phpmyadmin_3(g):
-    frame_url_selector = g.doc.select("id('frame_content')/@src")
-    if frame_url_selector.exists():
-        g.go(frame_url_selector.text())
+    return any(substring in href for substring, href in combinations)
 
 
 def download_sql_backup(url, user, password, dry_run=False, overwrite_existing=False, prepend_date=True, basename=None,
                         output_directory=os.getcwd(), exclude_dbs=None, compression='none', prefix_format=None,
                         timeout=60, http_auth=None, server_name=None, **kwargs):
     prefix_format = prefix_format or DEFAULT_PREFIX_FORMAT
-    exclude_dbs = exclude_dbs.split(',') or []
-    encoding = '' if compression == 'gzip' else 'gzip'
+    exclude_dbs = exclude_dbs.split(',') if exclude_dbs else []
+    session = requests.Session()
 
-    g = grab.Grab(encoding=encoding, timeout=timeout)
-    if http_auth:
-        g.setup(userpwd=http_auth)
-    else:
-        g.doc.set_input_by_id('input_username', user)
-        g.doc.set_input_by_id('input_password', password)
-        g.submit()
-    g.go(url)
-    if server_name:
-        g.doc.set_input_by_id('input_servername', server_name)
+    # Login
+    response = session.get(url, timeout=timeout)
+    if response.status_code != 200:
+        raise ValueError("Failed to load the login page.")
 
-    if not is_login_successful(g):
-        raise ValueError('Could not login - did you provide the correct username / password?')
+    tree = html.fromstring(response.content)
+    form_action = tree.xpath("//form[@id='login_form']/@action")
+    form_action = form_action[0] if form_action else url
 
-    open_frame_if_phpmyadmin_3(g)
+    form_data = {
+        "pma_username": user,
+        "pma_password": password,
+    }
 
-    export_url = g.doc.select("id('topmenu')//a[contains(@href,'server_export.php')]/@href").text()
-    g.go(export_url)
+    hidden_inputs = tree.xpath("//form[@id='login_form']//input[@type='hidden']")
+    for hidden_input in hidden_inputs:
+        name = hidden_input.get("name")
+        value = hidden_input.get("value", "")
+        if name:
+            form_data[name] = value
 
-    dbs_available = [option.attrib['value'] for option in g.doc.form.inputs['db_select[]']]
+    login_response = session.post(urljoin(url,form_action), data=form_data, timeout=timeout)
+
+    if login_response.status_code != 200:
+        raise ValueError("Could not log in. Please check your credentials.")
+
+    tree = html.fromstring(login_response.content)
+    if not is_login_successful(tree):
+        raise ValueError("Could not log in. Please check your credentials.")
+
+    # Extract export URL
+    export_url = tree.xpath("id('topmenu')//a[contains(@href,'server_export.php')]/@href")
+    if not export_url:
+        export_url = tree.xpath("id('topmenu')//a[contains(@href,'index.php?route=/server/export')]/@href")
+    if not export_url:
+        raise ValueError("Could not find export URL.")
+    export_url = export_url[0]
+
+    # Access export page
+    export_response = session.get(urljoin(url,export_url), timeout=timeout)
+    export_tree = html.fromstring(export_response.content)
+
+
+    # Determine databases to dump
+    dbs_available = export_tree.xpath("//select[@name='db_select[]']/option/@value")
     dbs_to_dump = [db_name for db_name in dbs_available if db_name not in exclude_dbs]
     if not dbs_to_dump:
-        print('Warning: no databases to dump (databases available: "{}")'.format('", "'.join(dbs_available)),
-            file=sys.stderr)
+        print(f'Warning: no databases to dump (databases available: "{", ".join(dbs_available)}")',
+              file=sys.stderr)
 
-    file_response = g.submit(
-        extra_post=[('db_select[]', db_name) for db_name in dbs_to_dump] + [('compression', compression)])
+    # Prepare form data
+    dump_form_action = export_tree.xpath("//form[@name='dump']/@action")[0]
+    form_data = {'db_select[]': dbs_to_dump}
+    form_data['compression'] = compression
+    form_data['what'] = 'sql'
+    form_data['filename_template'] = '@SERVER@'
+    form_data['sql_structure_or_data'] = 'structure_and_data'
+    dump_hidden_inputs = export_tree.xpath("//form[@name='dump']//input[@type='hidden']")
+    for hidden_input in dump_hidden_inputs:
+        name = hidden_input.get("name")
+        value = hidden_input.get("value", "")
+        if name:
+            form_data[name] = value
 
-    re_match = CONTENT_DISPOSITION_FILENAME_RE.match(g.doc.headers['Content-Disposition'])
+    # Submit form and download file
+    file_response = session.post(urljoin(url, dump_form_action), data=form_data, timeout=timeout, stream=True)
+    content_disposition = file_response.headers.get('Content-Disposition', '')
+    re_match = CONTENT_DISPOSITION_FILENAME_RE.match(content_disposition)
     if not re_match:
-        raise ValueError(
-            'Could not determine SQL backup filename from {}'.format(g.doc.headers['Content-Disposition']))
+        raise ValueError(f"Could not determine SQL backup filename from {content_disposition}")
 
     content_filename = re_match.group('filename')
     filename = content_filename if basename is None else basename + os.path.splitext(content_filename)[1]
@@ -97,16 +136,19 @@ def download_sql_backup(url, user, password, dry_run=False, overwrite_existing=F
     if os.path.isfile(out_filename) and not overwrite_existing:
         basename, ext = os.path.splitext(out_filename)
         n = 1
-        print('File {} already exists, to overwrite it use --overwrite-existing'.format(out_filename), file=sys.stderr)
+        print(f'File {out_filename} already exists, to overwrite it use --overwrite-existing', file=sys.stderr)
         while True:
-            alternate_out_filename = '{}_({}){}'.format(basename, n, ext)
+            alternate_out_filename = f'{basename}_({n}){ext}'
             if not os.path.isfile(alternate_out_filename):
                 out_filename = alternate_out_filename
                 break
             n += 1
 
+    # Save file if not dry run
     if not dry_run:
-        file_response.save(out_filename)
+        with open(out_filename, 'wb') as f:
+            for chunk in file_response.iter_content(chunk_size=8192):
+                f.write(chunk)
 
     return out_filename
 
